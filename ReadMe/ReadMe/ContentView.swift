6c3c7807532bdfa63112d1cbc99a50c77e2ae094
Emin Dağ <eemindag@gmail.com>
2022-06-07 21:55:24 +0300

    project created

---
+++ ReadMe/ReadMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReadMe
+//
+//  Created by Emin Dağ on 7.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReadMe/ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Emin Dağ on 7.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
